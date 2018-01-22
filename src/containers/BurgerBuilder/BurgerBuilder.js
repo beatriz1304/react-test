0276a49e6b0f6c20090945f5a866b643a32eacdd
Beatriz da Silveira <beatriz1304@gmail.com>
2018-01-22 13:04:58 -0200

    Implements build control

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 
 import Aux from '../../hoc/Aux';
-import Burger from '../../components/Burger/Burger';
+import Burger from '../../components/Burger/Burger;
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
 
@@ -19,7 +20,7 @@ class BurgerBuilder extends Component {
       <Aux>
         <div>
           <Burger ingredients={this.state.ingredients}/>
-          <div>Build Controls</div>
+          <BuildControls />
         </div>
       </Aux>
     );
