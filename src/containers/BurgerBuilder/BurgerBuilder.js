436be77fc4a4e3b0cf03c5ca7b4d00abbb01443d
Beatriz da Silveira <beatriz1304@gmail.com>
2018-02-19 13:07:06 -0300

    Disables ingredient button when ingredient count is less than 1

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -38,6 +38,11 @@ class BurgerBuilder extends Component {
 
   removeIngredientHandler = (type) => {
     const oldCount = this.state.ingredients[type];
+
+    if (oldCount <= 0) {
+      return;
+    }
+
     const updatedCount = oldCount - 1;
     const updatedIngredients = {
       ...this.state.ingredients
@@ -50,13 +55,21 @@ class BurgerBuilder extends Component {
   }
 
   render () {
+    const disabledInfo = {
+      ...this.state.ingredients
+    };
+    for (let key in disabledInfo) {
+      disabledInfo[key] = disabledInfo[key] <= 0
+    }
+
     return (
       <Aux>
         <div>
           <Burger ingredients={this.state.ingredients}/>
           <BuildControls
             ingredientAdded={this.addIngredientHandler}
-            ingredientRemoved={this.removeIngredientHandler}/>
+            ingredientRemoved={this.removeIngredientHandler}
+            disabled={disabledInfo}/>
         </div>
       </Aux>
     );
