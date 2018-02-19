8db380cf2c2fce0f8cdf917233cdca627337aa69
Beatriz da Silveira <beatriz1304@gmail.com>
2018-02-19 12:54:45 -0300

    Implements ingredient removing handler

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -37,7 +37,16 @@ class BurgerBuilder extends Component {
   }
 
   removeIngredientHandler = (type) => {
-
+    const oldCount = this.state.ingredients[type];
+    const updatedCount = oldCount - 1;
+    const updatedIngredients = {
+      ...this.state.ingredients
+    };
+    updatedIngredients[type] = updatedCount;
+    const priceDeduction = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice - priceDeduction;
+    this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
   }
 
   render () {
@@ -45,7 +54,9 @@ class BurgerBuilder extends Component {
       <Aux>
         <div>
           <Burger ingredients={this.state.ingredients}/>
-          <BuildControls ingredientAdded={this.addIngredientHandler}/>
+          <BuildControls
+            ingredientAdded={this.addIngredientHandler}
+            ingredientRemoved={this.removeIngredientHandler}/>
         </div>
       </Aux>
     );
